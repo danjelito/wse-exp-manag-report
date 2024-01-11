ebe17cb1e236f9d8f2c4ad8cc081bc68961f059b
Devan Anjelito <anjelitodevan@gmail.com>
2024-01-11 12:06:54 +0700

    move path to dotenv

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 import re
+import os
+from dotenv import load_dotenv
 
-month = "12 dec 2023"  # note: to find attendance data folder for current month
+load_dotenv()  # load secret env variables
+path_attendance_data = os.getenv("path_attendance_data")
+path_session_data = os.getenv("path_session_data")
+path_trainer_data = os.getenv("path_trainer_data")
+path_member_pop = os.getenv("path_member_pop")
+path_noncoco = os.getenv("path_noncoco")
+path_coco_member = os.getenv("path_coco_member")
+path_erwin_member = os.getenv("path_erwin_member")
+
+month = "12 dec 2023"  # to find attendance data folder for current month
 month_number = int(
     re.search("\d+", month).group()
-)  # note: to filter current month only in noncoco
-month_noncoco = month[3:].title()  # note: to find sheet name in noncoco
+)  # to filter current month only in noncoco
+month_noncoco = month[3:].title()  # to find sheet name in noncoco
